b386dc9925da80fc911bd25870ccd1fc1ea417b3
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-12-20 14:33:30 -0300

    Finish

---
+++ index.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+
+function mail () {
+    let transporter = nodemailer.createTransport({
+
+        host: 'smtp.gmail.com',
+        port:587,
+        secure: false,
+        auth:{
+            user: 'victorcesar2031@gmail.com',
+            pass: 'ikwsacjhbwcsbeil'
+        }
+    
+    });
+    
+    transporter.sendMail({
+    
+      from: 'Dron <victorcesar2031@gmail.com>',
+      to: 'victorcesar2031@gmail.com',
+      subject: 'Olá',
+      text: 'OLÁ, Dron',
+      html: '<h1>Você é um gênio</h1>'
+    
+    }).then(message=>{
+        console.log('email enviado');
+    }).catch(err=>{
+        console.log(err);
+    });
+    
+}
+setInterval(mail,10000);
